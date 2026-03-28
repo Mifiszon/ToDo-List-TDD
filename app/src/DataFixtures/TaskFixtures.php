f124f1deacd2ac823d55aa673477c4a1f3e5f50d
Michał Ogiba <mifiszon@gmail.com>
2026-03-28 21:27:06 +0100

    task

--- app/src/DataFixtures/TaskFixtures.php
+++ app/src/DataFixtures/TaskFixtures.php
@@ -1,46 +1,34 @@
 <?php
 /**
- * Base fixtures.
+ * Task fixtures.
  */
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
-use Faker\Generator;
+use App\Entity\Task;
 
 /**
- * Class AbstractBaseFixtures.
+ * Class TaskFixtures.
  */
-abstract class AbstractBaseFixtures extends Fixture
+class TaskFixtures extends AbstractBaseFixtures
 {
-    /**
-     * Faker.
-     */
-    protected Generator $faker;
-
-    /**
-     * Persistence object manager.
-     *
-     * @var ObjectManager
-     */
-    protected ObjectManager $manager;
-
-    /**
-     * Load.
-     *
-     * @param ObjectManager $manager Persistence object manager
-     */
-    public function load(ObjectManager $manager): void
-    {
-        $this->manager = $manager;
-        $this->faker = Factory::create();
-        $this->loadData();
-    }
-
     /**
      * Load data.
      */
-    abstract protected function loadData(): void;
+    public function loadData(): void
+    {
+        for ($i = 0; $i < 10; ++$i) {
+            $task = new Task();
+            $task->setTitle($this->faker->sentence);
+            $task->setCreatedAt(
+                \DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+            );
+            $task->setUpdatedAt(
+                \DateTimeImmutable::createFromMutable($this->faker->dateTimeBetween('-100 days', '-1 days'))
+            );
+            $this->manager->persist($task);
+        }
+
+        $this->manager->flush();
+    }
 }

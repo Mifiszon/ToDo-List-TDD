d97bb3a5d35fe0ed14d57133e082bcc0cc6cf4c9
Michał Ogiba <mifiszon@gmail.com>
2026-04-17 13:41:24 +0200

    avatar edit

--- app/src/Service/AvatarService.php
+++ app/src/Service/AvatarService.php
@@ -6,38 +6,61 @@
 namespace App\Service;
 
 use App\Entity\Avatar;
+use App\Entity\User;
 use App\Repository\AvatarRepository;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * Class Avatar service.
+ * Class AvatarService.
  */
 class AvatarService implements AvatarServiceInterface
 {
     /**
      * Constructor.
      *
+     * @param string                     $targetDirectory   Target directory
      * @param AvatarRepository           $avatarRepository  Avatar repository
      * @param FileUploadServiceInterface $fileUploadService File upload service
+     * @param Filesystem                 $filesystem        Filesystem component
      */
-    public function __construct(private readonly AvatarRepository $avatarRepository, private readonly FileUploadServiceInterface $fileUploadService)
+    public function __construct(private readonly string $targetDirectory, private readonly AvatarRepository $avatarRepository, private readonly FileUploadServiceInterface $fileUploadService, private readonly Filesystem $filesystem)
     {
     }
 
     /**
      * Create avatar.
      *
-     * @param UploadedFile  $uploadedFile Uploaded file
-     * @param Avatar        $avatar       Avatar entity
-     * @param UserInterface $user         User interface
+     * @param UploadedFile $uploadedFile Uploaded file
+     * @param Avatar       $avatar       Avatar entity
+     * @param User         $user         User entity
      */
-    public function create(UploadedFile $uploadedFile, Avatar $avatar, UserInterface $user): void
+    public function create(UploadedFile $uploadedFile, Avatar $avatar, User $user): void
     {
         $avatarFilename = $this->fileUploadService->upload($uploadedFile);
 
         $avatar->setUser($user);
         $avatar->setFilename($avatarFilename);
         $this->avatarRepository->save($avatar);
+    }
+
+    /**
+     * Update avatar.
+     *
+     * @param UploadedFile $uploadedFile Uploaded file
+     * @param Avatar       $avatar       Avatar entity
+     * @param User         $user         User entity
+     */
+    public function update(UploadedFile $uploadedFile, Avatar $avatar, User $user): void
+    {
+        $filename = $avatar->getFilename();
+
+        if (null !== $filename) {
+            $this->filesystem->remove(
+                $this->targetDirectory.'/'.$filename
+            );
+
+            $this->create($uploadedFile, $avatar, $user);
+        }
     }
 }
--- app/src/Service/AvatarServiceInterface.php
+++ app/src/Service/AvatarServiceInterface.php
@@ -22,4 +22,13 @@ interface AvatarServiceInterface
      * @param User         $user         User entity
      */
     public function create(UploadedFile $uploadedFile, Avatar $avatar, User $user): void;
+
+    /**
+     * Update avatar.
+     *
+     * @param UploadedFile $uploadedFile Uploaded file
+     * @param Avatar       $avatar       Avatar entity
+     * @param User         $user         User interface
+     */
+    public function update(UploadedFile $uploadedFile, Avatar $avatar, User $user): void;
 }
